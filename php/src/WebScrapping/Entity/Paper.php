a11dbabccdc6b379e8aaf6de31071f814cbaa479
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2024-05-06 12:57:27 -0300

    create paper entity

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -5,40 +5,17 @@ namespace Chuva\Php\WebScrapping\Entity;
 /**
  * The Paper class represents the row of the parsed data.
  */
-class Paper {
+class Paper
+{
 
-  /**
-   * Paper Id.
-   *
-   * @var int
-   */
   public $id;
-
-  /**
-   * Paper Title.
-   *
-   * @var string
-   */
   public $title;
-
-  /**
-   * The paper type (e.g. Poster, Nobel Prize, etc).
-   *
-   * @var string
-   */
   public $type;
-
-  /**
-   * Paper authors.
-   *
-   * @var \Chuva\Php\WebScrapping\Entity\Person[]
-   */
   public $authors;
 
-  /**
-   * Builder.
-   */
-  public function __construct($id, $title, $type, $authors = []) {
+
+  public function __construct($id, $title, $type, $authors = [])
+  {
   }
 
 }
